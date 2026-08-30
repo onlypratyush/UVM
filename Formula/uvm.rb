ee8b2ca5a81ff0ae9d03ccf06a9aa017b61da855
Pratyush Karn <pratyush.k@teamwebmark.com>
2026-08-30 23:07:27 +0530

    fix(hashes): sync Homebrew and Scoop manifests with GitHub release v0.0.5 assets

--- Formula/uvm.rb
+++ Formula/uvm.rb
@@ -7,20 +7,20 @@ class Uvm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_arm64.tar.gz"
-      sha256 "b9aa6e5e2e2f2992125dc906e650bbbfe7ae2fb3a3bad1948425ed275760665a"
+      sha256 "5984435ad5e01e9f6e2f5e786bb9760f9871081758f54dab23c626ffcadb50ed"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_amd64.tar.gz"
-      sha256 "f421b2bb3c9b44044bc12865c3e04893473e717d5c207cfbbd246a6e06b3f0f1"
+      sha256 "58201c0c025f2be93ed2d56ed6dc005eacc62bdb1006e56726d74da1e9e106cf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_arm64.tar.gz"
-      sha256 "1a1f62fa9768747251f21af2f6364db1cbb306b8f8b7641a74ca5f7b92105ab6"
+      sha256 "d20fcfc80a8f020cb4cad75ba4c3f4b6c9c19489de34bc964d4c9ca1ddc0cc73"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_amd64.tar.gz"
-      sha256 "d0240867d47bc38c9429649179d8f10cff92f665652f612877b7266f06b01840"
+      sha256 "3fb94f7fda6de4702518b46b132864de8fce80c9ea5d1697374d1156298d0ad6"
     end
   end
 
