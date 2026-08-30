fe5ef5e000f8480020463610a85c98ef3e797d90
Pratyush Karn <pratyush.k@teamwebmark.com>
2026-08-30 20:22:44 +0530

    chore(homebrew): add sha256 checksums to Formula/uvm.rb

--- Formula/uvm.rb
+++ Formula/uvm.rb
@@ -7,16 +7,20 @@ class Uvm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_arm64.tar.gz"
+      sha256 "d2af72e30b865718f7e03af6bf8987ec9cb0300df34aa0fae466d785a9c36938"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_amd64.tar.gz"
+      sha256 "805e4a3ade8dde336855eb2434df9c13109b237fdb3d2c04dee43f05a9bcede2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_arm64.tar.gz"
+      sha256 "52a37599bfd8ab5134a3234b9dc871619ca95002d771247b3835a2e6285dcf5b"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_amd64.tar.gz"
+      sha256 "6322286a57a296865d4874540033a3849cb5bfe7704256d54c86da7d2b88bcc2"
     end
   end
 
