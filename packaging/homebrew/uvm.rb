48ed9510a071700d24c3541ce4c6857ea87d6821
Pratyush Karn <pratyush.k@teamwebmark.com>
2026-08-30 22:58:01 +0530

    fix(packaging): update release v0.0.5 sha256 checksums

--- packaging/homebrew/uvm.rb
+++ packaging/homebrew/uvm.rb
@@ -7,20 +7,20 @@ class Uvm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_arm64.tar.gz"
-      sha256 "6fc1aa342edb5034f10e467c15c46448d33d7bf7d070f08a78097a222615527d"
+      sha256 "b9aa6e5e2e2f2992125dc906e650bbbfe7ae2fb3a3bad1948425ed275760665a"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_darwin_amd64.tar.gz"
-      sha256 "a30c642453daf553acd39413bb9c30ff175f1d2f3557a57d75d4389072bd661d"
+      sha256 "f421b2bb3c9b44044bc12865c3e04893473e717d5c207cfbbd246a6e06b3f0f1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_arm64.tar.gz"
-      sha256 "d66832f7acdbff16c3a1a6fa2e235a9931f9a9e10f7d84c28088c43d75725fca"
+      sha256 "1a1f62fa9768747251f21af2f6364db1cbb306b8f8b7641a74ca5f7b92105ab6"
     else
       url "https://github.com/onlypratyush/UVM-/releases/download/v#{version}/uvm_v#{version}_linux_amd64.tar.gz"
-      sha256 "9ef02302679be0210e17620d35cbc0fffe00e729d545ab55dabe0f75cbe36bdd"
+      sha256 "d0240867d47bc38c9429649179d8f10cff92f665652f612877b7266f06b01840"
     end
   end
 
